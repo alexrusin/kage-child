ebfa9915b4a6dcd7b19d67b9fb083b8ce9dce8b4
Alex Rusin <alexrusin_2000@yahoo.com>
2016-08-15 09:14:40 -0700

    Add extended valid elements to TinyMCE editor in functions.php

--- functions.php
+++ functions.php
@@ -15,3 +15,27 @@ function kage_child_enqueue_scripts(){
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', false, '1.0', 'all' );
 }
 add_action('wp_enqueue_scripts', 'kage_child_enqueue_scripts');
+
+// Prevent TinyMCE from stripping out html
+function schema_TinyMCE_init($in)
+{
+    /**
+     *   Edit extended_valid_elements as needed. For syntax, see
+     *   http://www.tinymce.com/wiki.php/Configuration:valid_elements
+     *   https://www.tinymce.com/docs/configure/content-filtering/
+     *
+     *   NOTE: Adding an element to extended_valid_elements will cause TinyMCE to ignore
+     *   default attributes for that element.
+     *   Eg. a[title] would remove href unless included in new rule: a[title|href]
+     */
+    if(!empty($in['extended_valid_elements']))
+        $in['extended_valid_elements'] .= ',';
+
+    $in['extended_valid_elements'] .= 'div[id|style|title|itemscope|itemtype],';
+    $in['extended_valid_elements'] .= 'img[class|src|alt|width|height|itemprop],';
+    $in['extended_valid_elements'] .= 'span[itemprop],';
+    $in['extended_valid_elements'] .= 'meta[itemprop]';
+
+    return $in;
+}
+add_filter('tiny_mce_before_init', 'schema_TinyMCE_init' );
